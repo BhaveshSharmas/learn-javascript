dbdb4caec5aa358dc11e1bec56845df17e3dacf8
Bhavesh Sharmas <bhaveshsharmas@protonmail.com>
2025-02-06 13:44:44 +0000

    datatypes and variable

---
+++ basics/variables.js
@@ -0,0 +1,7 @@
+const accoutnId = 12345
+let accountName = "Bhavesh"
+var accountEmail = "Bhaveshgmail.com"
+
+console.log(accountEmail)
+accountName = "Bhavesh sharma"
+console.table([accoutnId,accountName,accountEmail])
