dfe9538172bca906d790d3291c301c76797192c3
Bhavesh Sharmas <bhaveshsharmas@protonmail.com>
2025-02-10 09:52:36 +0000

    add Strings and tests

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,40 @@
-console.log("Hello from this side")
+// console.log("Hello from this side")
+// const heros = ['one','two','three']
+// console.log(heros)
+
+// const myFunc = function(){
+//     console.log("test")
+// }
+
+// console.log(myFunc)
+
+// let myObj = {
+//     age: "12",
+//     name: "Bhavesh"
+// }
+
+// myObj.name = "bhaveshs"
+// console.log(myObj.name)
+
+// let name = "bhavesh"
+// let alsoName = name
+// console.log(alsoName)
+// alsoName = "devs"
+// console.log(alsoName)
+
+// let name = "bhavesh"
+// let sur = "sharma"
+
+// console.log(`My name is ${name} and surname is ${sur}`)
+// console.log(`${name} ${sur}`)
+
+let st = 'bhavesh'
+let name = new String('bhavesh')
+console.log(st.length);
+console.log(name);
+console.log(st);
+console.log(st.split('a'));
+console.log(st.includes('a'))
+console.log(st.substring(0,4));
+console.log(st.indexOf('a'));
+
