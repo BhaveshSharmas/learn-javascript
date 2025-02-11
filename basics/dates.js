87556c22018f95858023bb7ce09edd8086f47dc0
Bhavesh Sharmas <bhaveshsharmas@protonmail.com>
2025-02-11 07:07:10 +0000

    1 Arrays

---
+++ basics/dates.js
@@ -0,0 +1,12 @@
+let mdate = new Date()
+// console.log(mdate);
+// console.log(mdate.toDateString());
+
+let mydate = new Date("2025-02-10")
+console.log(mydate.toLocaleDateString());
+
+let mydates = new Date("02-10-2025")
+console.log(mydates.toLocaleDateString());
+
+console.log(mydate.getTime());
+
